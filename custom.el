d4c292281a420e6f74b44ff1ff955e3aa396a8a5
Mike Heffner <mikeh@fesnel.com>
2009-08-11 12:31:48 -0400

    * Fix c/c++ mode indents to be BSD friendly.

--- custom.el
+++ custom.el
@@ -3,3 +3,25 @@
 (define-key global-map [(f5)] 'buffer-menu)
 
 (define-key global-map [(meta g)] 'goto-line)
+
+;; Set whitespace column
+(setq whitespace-line-column 80)
+
+;; Set level for font decoration
+(setq font-lock-maximum-decoration
+      '((c-mode . 2) (c++-mode . 2)))
+
+;; Mode hook to set indent-tab-mode
+(defun load-c-settings ()
+  "Load custom C mode settings"
+  (c-set-style "bsd")
+  (setq tab-always-indents t)
+  (setq indent-tabs-mode t)
+  )
+
+(add-hook 'c-mode-hook
+          'load-c-settings
+          )
+(add-hook 'c++-mode-hook
+          'load-c-settings
+          )
