15a2bcf6deae1e81f53088438ad0a5f6e85d34c8
Mike Heffner <mikeh@fesnel.com>
2009-08-11 11:36:13 -0400

    * Add custom file.

---
+++ custom.el
@@ -0,0 +1,5 @@
+(define-key global-map [(f1)] 'delete-other-windows)
+(define-key global-map [(f2)] 'menu-bar-mode)
+(define-key global-map [(f5)] 'buffer-menu)
+
+(define-key global-map [(meta g)] 'goto-line)
