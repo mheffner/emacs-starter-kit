1685c2c0867e67a52405c636441cb290df360cc0
Mike Heffner <mikeh@fesnel.com>
2009-08-24 14:30:55 -0400

    * Set default c-style. * Don't align arguments to functions.

--- custom.el
+++ custom.el
@@ -40,6 +40,9 @@
   (c-set-style "bsd")
   (setq c-tab-always-indent nil)
   (setq indent-tabs-mode t)
+  (setq c-offsets-alist (quote ((arglist-cont . *)
+                                (arglist-cont-nonempty . *))))
+  (message "Loaded custom C settings")
   )
 
 (add-hook 'c-mode-hook
@@ -101,3 +104,10 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#ece9e9" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 107 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(c-default-style (quote ((c-mode . "bsd") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
+ )
