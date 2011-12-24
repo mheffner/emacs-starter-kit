6a60c02d5f9af983a06ce69a82e97faedf958508
Mike Heffner <mikeh@fesnel.com>
2011-12-24 10:22:15 -0500

    Add coffeescript mode

--- custom.el
+++ custom.el
@@ -125,6 +125,20 @@
 (add-hook 'ruby-mode-hook 'turn-on-ws-trim)
 
 ;;
+;; coffee-mode
+;;
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+;;
 ;; whitespace (display)
 ;;
 (require 'whitespace)
