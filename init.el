1142a68fd709ed44e0b2a20be2aa6676c841a62f
Mike Heffner <mike@librato.com>
2015-11-27 14:39:08 -0500

    Move lisp under lisp/ to avoid startup warnings.

--- init.el
+++ init.el
@@ -22,9 +22,9 @@
 
 ;; Load up ELPA, the package manager
 
-(add-to-list 'load-path dotfiles-dir)
-
+;; (add-to-list 'load-path dotfiles-dir)
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+(add-to-list 'load-path (concat dotfiles-dir "/lisp"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
